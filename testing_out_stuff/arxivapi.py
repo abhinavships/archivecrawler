d3947fb71e4943026af50aa6212c7213c817a863
abhinavships <abhinavships@users.noreply.github.com>
2026-08-13 07:59:19 +0530

    feat: update arxiv API search query and add frontend page

--- testing_out_stuff/arxivapi.py
+++ testing_out_stuff/arxivapi.py
@@ -1,4 +1,4 @@
-import urllib, urllib.request
-url = 'http://export.arxiv.org/api/query?search_query=all:selfattention_transformers&start=0&max_results=1'
-data = urllib.request.urlopen(url)
-print(data.read().decode('utf-8'))
+import urllib.request as libreq
+with libreq.urlopen('http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=10') as url:
+      r = url.read()
+print(r)
